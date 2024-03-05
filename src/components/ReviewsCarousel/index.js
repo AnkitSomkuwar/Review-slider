8b3819e1de46de68c72df1c8a586cb814fec7c24
ankit <somkankit998@gmail.com>
2024-03-05 17:52:20 +0000

    solution

---
+++ src/components/ReviewsCarousel/index.js
@@ -0,0 +1,68 @@
+// Write your code here
+import './index.css'
+import {Component} from 'react'
+
+class ReviewsCarousel extends Component {
+  state = {
+    activeReview: 0,
+  }
+
+  onLeftReview = () => {
+    const {activeReview} = this.state
+
+    if (activeReview > 0) {
+      this.setState(prevState => ({
+        activeReview: prevState.activeReview - 1,
+      }))
+    }
+  }
+
+  onRightReview = () => {
+    const {activeReview} = this.state
+    const {reviewsList} = this.props
+    if (activeReview < reviewsList.length - 1) {
+      this.setState(prevState => ({
+        activeReview: prevState.activeReview + 1,
+      }))
+    }
+  }
+
+  render() {
+    const {reviewsList} = this.props
+    const {activeReview} = this.state
+    const {imgUrl, username, companyName, description} =
+      reviewsList[activeReview]
+
+    return (
+      <div className="bg-container">
+        <div className="review-container">
+          <h1 className="heading">Reviews</h1>
+          <div className="curosal-container">
+            <button
+              type="button"
+              testid="leftArrow"
+              className="left-arrow"
+              onClick={this.onLeftReview}
+            >
+              <img src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png" alt = "left arrow" />
+            </button>
+            <img src={imgUrl} alt="review img" />
+            <button
+              type="button"
+              testid="rightArrow"
+              className="right-arrow"
+              onClick={this.onRightReview}
+            >
+              <img src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png" alt = "right arrow" />
+            </button>
+            <p className="name">{username}</p>
+            <p className="company-name">{companyName}</p>
+            <p className="description">{description}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ReviewsCarousel
